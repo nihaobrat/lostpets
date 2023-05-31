d233c168353c47af9afac612ab750f99b9244081
Mary <maryamfrolova@yandex.ru>
2023-05-31 03:54:00 +0300

    1

---
+++ templates/Qr_code.py
@@ -0,0 +1,13 @@
+import qrcode
+
+
+def qrcreate():
+    # import url
+    res = str(input())
+    img = qrcode.make(res)
+    type(img)
+#Enter image № for creating qr code
+    img.save("qr7.png")
+
+
+qrcreate()
